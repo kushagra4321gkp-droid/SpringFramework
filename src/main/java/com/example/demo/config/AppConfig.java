877e1ff8523fd7c02725e8b7448aadb6b24fa0d8
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-16 19:05:50 +0530

    @Primary and @Qualifier(' ')

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -3,15 +3,18 @@ package com.example.demo.config;
 import com.example.demo.Computer;
 import com.example.demo.Desktop;
 import com.example.demo.Human;
+import com.example.demo.Laptop;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public Human human(Computer com){
+    public Human human(@Qualifier("laptop") Computer com){
         Human h1 = new Human();
         h1.setAge(23);
         h1.setCom(com);
@@ -22,7 +25,13 @@ public class AppConfig {
 //    @Bean({"com1", "desktop", "Beast"})
     @Bean
 //    @Scope("prototype")
+    @Primary
     public Desktop desktop(){
         return new Desktop();
+    }
+
+    @Bean
+    public Laptop laptop(){
+        return new Laptop();
     }
 }
