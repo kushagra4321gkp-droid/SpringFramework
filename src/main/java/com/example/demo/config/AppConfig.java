48247367ec7829b1730353d86596d9f13203afc8
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-16 19:49:46 +0530

    Using Streotype Annotation -> @Component, @ComponentScan('name of the package where Spring has to search for @Components to avoid Manual Config

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -5,33 +5,36 @@ import com.example.demo.Desktop;
 import com.example.demo.Human;
 import com.example.demo.Laptop;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan("com.example.demo")
 public class AppConfig {
 
-    @Bean
-    public Human human(@Qualifier("laptop") Computer com){
-        Human h1 = new Human();
-        h1.setAge(23);
-        h1.setCom(com);
-        return h1;
-    }
+//    @Bean
+//    public Human human(@Qualifier("laptop") Computer com){
+//        Human h1 = new Human();
+//        h1.setAge(23);
+//        h1.setCom(com);
+//        return h1;
+//    }
+//
+//
+////    @Bean({"com1", "desktop", "Beast"})
+//    @Bean
+////    @Scope("prototype")
+//    @Primary
+//    public Desktop desktop(){
+//        return new Desktop();
+//    }
+//
+//    @Bean
+//    public Laptop laptop(){
+//        return new Laptop();
+//    }
 
 
-//    @Bean({"com1", "desktop", "Beast"})
-    @Bean
-//    @Scope("prototype")
-    @Primary
-    public Desktop desktop(){
-        return new Desktop();
-    }
-
-    @Bean
-    public Laptop laptop(){
-        return new Laptop();
-    }
 }
+
+
+// Stereotype annotation, primarily in the Spring Framework, marks a class with a specific role (like @Service, @Repository) for automatic component detection and management by the Spring container, simplifying configuration and improving code readability by defining a component's purpose (e.g., business logic, data access). These annotations are specialized versions of the base @Component, automatically registering beans without manual XML or Java configuration
